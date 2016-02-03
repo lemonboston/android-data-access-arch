c5515f4d84a881437c013099e9a1ada5c24d82dc
Gabor Keszthelyi <gabor_keszthelyi@epam.com>
2016-02-03 15:27:34 +0100

    Use DataBinding in service selector view.

--- app/src/main/java/com/gk/daas/dialog/ServiceSelectorDialog.java
+++ app/src/main/java/com/gk/daas/dialog/ServiceSelectorDialog.java
@@ -1,67 +1,45 @@
 package com.gk.daas.dialog;
 
-import android.app.Activity;
 import android.app.Dialog;
-import android.view.View;
-import android.widget.RadioButton;
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.view.LayoutInflater;
 
 import com.gk.daas.R;
+import com.gk.daas.databinding.MockRealServiceSelectorBinding;
 import com.gk.daas.di.DebugOptions;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 /**
  * @author Gabor_Keszthelyi
  */
 public class ServiceSelectorDialog {
 
-    private final Activity activity;
-
-    @Bind(R.id.Radio_RealService)
-    RadioButton realServiceButton;
-
-    @Bind(R.id.Radio_MockService)
-    RadioButton mockServiceButton;
+    private final Context context;
 
     private Dialog dialog;
 
-    public ServiceSelectorDialog(Activity activity) {
-        this.activity = activity;
+    public ServiceSelectorDialog(Context context) {
+        this.context = context;
     }
 
     public void show() {
-        View layout = activity.getLayoutInflater().inflate(R.layout.mock_real_service_selector, null);
-        ButterKnife.bind(this, layout);
-
-        dialog = new Dialog(activity);
-        dialog.setContentView(layout);
-
+        MockRealServiceSelectorBinding binding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.mock_real_service_selector, null, false);
         if (DebugOptions.MOCK_WEATHER_SERVICE) {
-            mockServiceButton.setChecked(true);
+            binding.MockService.setChecked(true);
         } else {
-            realServiceButton.setChecked(true);
+            binding.RealService.setChecked(true);
         }
+        binding.MockService.setOnClickListener(v -> setMockServiceAndDismiss(true, dialog));
+        binding.RealService.setOnClickListener(v -> setMockServiceAndDismiss(false, dialog));
 
+        dialog = new Dialog(context);
+        dialog.setContentView(binding.getRoot());
         dialog.show();
     }
 
-    @OnClick(R.id.Radio_RealService)
-    void onRealServiceRadioButtonClick() {
-        DebugOptions.MOCK_WEATHER_SERVICE = false;
+    private void setMockServiceAndDismiss(boolean isMockService, Dialog dialog) {
+        DebugOptions.MOCK_WEATHER_SERVICE = isMockService;
         dialog.dismiss();
     }
 
-    @OnClick(R.id.Radio_MockService)
-    void onMockServiceRadioButtonClick() {
-        DebugOptions.MOCK_WEATHER_SERVICE = true;
-        dialog.dismiss();
-    }
-
-    // TODO create proxy service that decides dynamically runtime, so no need to restart activity
-    private void restartActivity() {
-        activity.finish();
-        activity.startActivity(activity.getIntent());
-    }
 }
