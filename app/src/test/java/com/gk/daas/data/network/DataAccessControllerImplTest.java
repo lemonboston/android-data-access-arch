e5e28855d84f4d4dd8fd68c4f7308bf5f6f1b638
Gabor Keszthelyi <gabor_keszthelyi@epam.com>
2016-02-15 19:08:36 +0100

    Add empty test.

---
+++ app/src/test/java/com/gk/daas/data/network/DataAccessControllerImplTest.java
@@ -0,0 +1,15 @@
+package com.gk.daas.data.network;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Gabor_Keszthelyi
+ */
+public class DataAccessControllerImplTest {
+
+    @Test
+    public void test() {
+        Assert.assertTrue(true);
+    }
+}
