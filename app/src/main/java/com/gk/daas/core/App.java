ae9611fb36f9f752a1ff0f27eab2e12ae2bf3442
Gabor Keszthelyi <gabor_keszthelyi@epam.com>
2016-05-09 19:01:18 +0200

    Upgrade gradle plugin version and remove databinding and dagger2metrics to fix build.

--- app/src/main/java/com/gk/daas/core/App.java
+++ app/src/main/java/com/gk/daas/core/App.java
@@ -3,7 +3,6 @@ package com.gk.daas.core;
 import android.app.Application;
 
 import com.facebook.stetho.Stetho;
-import com.frogermcs.dagger2metrics.Dagger2Metrics;
 import com.gk.daas.di.Injector;
 
 /**
@@ -14,9 +13,6 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        if (Config.ENABLE_DAGGER2METRICS) {
-            Dagger2Metrics.enableCapturing(this);
-        }
 
         Injector.init(this);
 
--- app/src/main/java/com/gk/daas/dialog/ServiceSelectorDialog.java
+++ app/src/main/java/com/gk/daas/dialog/ServiceSelectorDialog.java
@@ -2,12 +2,12 @@ package com.gk.daas.dialog;
 
 import android.app.Dialog;
 import android.content.Context;
-import android.databinding.DataBindingUtil;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RadioButton;
 
 import com.gk.daas.R;
 import com.gk.daas.core.Config;
-import com.gk.daas.databinding.DialogServiceSelectorBinding;
 import com.gk.daas.framework.access.Toaster;
 
 /**
@@ -26,16 +26,19 @@ public class ServiceSelectorDialog {
     public void show() {
         Dialog dialog = new Dialog(context);
 
-        DialogServiceSelectorBinding binding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.dialog_service_selector, null, false);
-        if (Config.MOCK_WEATHER_SERVICE) {
-            binding.MockService.setChecked(true);
-        } else {
-            binding.RealService.setChecked(true);
-        }
-        binding.MockService.setOnClickListener(v -> setMockServiceAndDismiss(true, dialog));
-        binding.RealService.setOnClickListener(v -> setMockServiceAndDismiss(false, dialog));
+        View layout = LayoutInflater.from(context).inflate(R.layout.dialog_service_selector, null);
+        RadioButton mockServiceButton = (RadioButton) layout.findViewById(R.id.MockService);
+        RadioButton realServiceButton = (RadioButton) layout.findViewById(R.id.RealService);
 
-        dialog.setContentView(binding.getRoot());
+        if (Config.MOCK_WEATHER_SERVICE) {
+            mockServiceButton.setChecked(true);
+        } else {
+            realServiceButton.setChecked(true);
+        }
+        mockServiceButton.setOnClickListener(v -> setMockServiceAndDismiss(true, dialog));
+        realServiceButton.setOnClickListener(v -> setMockServiceAndDismiss(false, dialog));
+
+        dialog.setContentView(layout);
         dialog.show();
     }
 
